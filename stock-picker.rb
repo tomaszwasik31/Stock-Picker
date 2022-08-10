90208a29eced6c59de8371d764bac328f0aa6ee7
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-08-10 10:08:49 +0100

    comments, clean up

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,24 +1,21 @@
 
 def stock_picker(array)
 
- 
+ #all combination
   all_com= array.combination(2).to_a
-
-      buy_before = all_com.select{|a,b| a<b }
-     
+#buy before sell validation
+    buy_before = all_com.select{|a,b| a<b }
+     #subtraction
          array_profits= buy_before.map {|e| e.inject(:-)}
-     
+     #find lowest value
         best_profit_value = array_profits.min
-      
+      #find index with this value
          i=array_profits.find_index(best_profit_value)
+         #use this index again
   best_pair = buy_before[i]
- 
-
-
-
-   p best_pair.map {|i| array.find_index(i) } 
+#print index'es of this value's
+  p best_pair.map {|i| array.find_index(i) } 
   
-
 end
 
 
